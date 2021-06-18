b35c9e2bac2784a1ce46e32baf13735c076af04e
Hassan Bhatti <hasha361@gmail.com>
2021-06-18 19:57:26 +0500

    Update imageUpload.js

--- client/src/utils/imageUpload.js
+++ client/src/utils/imageUpload.js
@@ -23,10 +23,10 @@ export const imageUpload = async (images) => {
             formData.append("file", item)
         }
         
-        formData.append("upload_preset", "efxjficn")
-        formData.append("cloud_name", "devat-channel")
+        formData.append("upload_preset", "ij9axjtt")
+        formData.append("cloud_name", "bokking")
 
-        const res = await fetch("https://api.cloudinary.com/v1_1/devat-channel/upload", {
+        const res = await fetch("https://api.cloudinary.com/v1_1/bokking/image/upload", {
             method: "POST",
             body: formData
         })
